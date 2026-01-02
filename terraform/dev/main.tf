2686d74e5b925c16211627d10c910e0208cd9ce5
Brahmanya <98081415+brahmanyasudulagunta@users.noreply.github.com>
2026-01-02 16:54:58 -0600

    update

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -21,7 +21,7 @@ module "dev_namespace" {
   name   = "develop"
 }
 
-module "test_namespace" {
-  source = "../modules/namespace"
-  name   = "test-jenkins"
-}
+# module "test_namespace" {
+#  source = "../modules/namespace"
+#  name   = "test-jenkins"
+# }
