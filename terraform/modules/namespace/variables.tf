84a77cee312508624cd163b418fe89d01bfd5f4c
Brahmanya <98081415+brahmanyasudulagunta@users.noreply.github.com>
2026-01-27 14:15:40 -0600

    variables.tf

--- terraform/modules/namespace/variables.tf
+++ terraform/modules/namespace/variables.tf
@@ -1,4 +1,14 @@
 variable "name" {
   description = "Namespace name"
   type        = string
+  
+  validation {
+    condition = can(
+      regex(
+        "^[a-z0-9]([-a-z0-9]*[a-z0-9])?$",
+        var.name
+      )
+    )
+    error_message = "Namespace name must be lowercase RFC 1123 compliant (e.g. team-alpha)."
+  }
 }
