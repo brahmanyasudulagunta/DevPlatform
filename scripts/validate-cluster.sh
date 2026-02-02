15d9ab2fb3fb2381bb79b34cbeeda581f1ca84d4
Brahmanya <98081415+brahmanyasudulagunta@users.noreply.github.com>
2026-02-02 12:27:00 -0600

    validate

--- scripts/validate-cluster.sh
+++ scripts/validate-cluster.sh
@@ -4,7 +4,7 @@ echo "Checking nodes..."
 kubectl get nodes || exit 1
 
 echo "Checking namespaces..."
-kubectl get ns | grep -E "dev|staging|prod" || exit 1
+kubectl get ns | grep -E "develop|staging|production" || exit 1
 
 echo "Checking system pods..."
 kubectl get pods -n kube-system || exit 1
